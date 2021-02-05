0594a6737ff9349a0be951438033865af227f3a6
raku005 <$raku005@AUTH.hpicorp.net>
2021-02-05 07:41:30 +0000

    commit for FY21

---
+++ Scripts/TS_003 - Copy (6)/Action.c
@@ -0,0 +1,13 @@
+Action()
+{
+	web_custom_request("web_custom_request",
+		"URL=https://blazedemo.com/",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	return 0;
+}
